706f357390ea8f04226c13fcb85239bb3f24dbfc
Kevin Pei <kpei98@gmail.com>
2022-03-17 02:50:33 -0700

    support dark mode

--- src/lib/MonacoEditor.tsx
+++ src/lib/MonacoEditor.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import Editor, {
   BeforeMount,
   EditorProps,
@@ -6,6 +6,8 @@ import Editor, {
 } from "@monaco-editor/react";
 
 type MountArgs = Parameters<OnMount>;
+
+const colorSchemeQueryList = window.matchMedia("(prefers-color-scheme: dark)");
 
 export default function (props: EditorProps) {
   // handle resizing of window
@@ -23,12 +25,19 @@ export default function (props: EditorProps) {
     return () => window.removeEventListener("resize", onResize);
   });
 
+  const [mediaQuery, setMediaQuery] = useState(colorSchemeQueryList);
+  useEffect(() => {
+    const onChange = (newScheme) => setMediaQuery(newScheme);
+    colorSchemeQueryList.addEventListener("change", onChange);
+    return () => colorSchemeQueryList.removeEventListener("change", onChange);
+  }, []);
+
   return (
     <Editor
       {...props}
       path="file.jsx"
       language="javascript"
-      theme="vs-light"
+      theme={mediaQuery.matches ? "vs-dark" : "vs-light"}
       onMount={onMount}
     />
   );
